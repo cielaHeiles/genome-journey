b151cb504942a907960a3f7fa34bb8d79e527544
cielaHeiles <ciela.heiles@icloud.com>
2025-12-05 10:52:21 -0700

    Pushing new changes to main and player files

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 #include "tile.cpp"
 #include "player.cpp"
 int main(){
-    readCharacterFile();
+    vector<Character> selectedCharacters = readCharacterFile();
+    for(int i = 0; i<2; i++){
+        cout<<selectedCharacters[i].name;
+        cout<<selectedCharacters[i].experience;
+        cout<<selectedCharacters[i].accuracy;
+        cout<<selectedCharacters[i].efficiency;
+        cout<<selectedCharacters[i].insight;
+        cout<<selectedCharacters[i].discoveryPoints;
+    }
     return 0;
 }
--- Player.cpp
+++ Player.cpp
@@ -83,7 +83,7 @@ struct Character{
     int insight;
     int discoveryPoints;
 };
-void readCharacterFile(){
+vector<Character> readCharacterFile(){
     
     //Read individual lines from characters.txt into characters vector
     //Read vector into 2D array
@@ -101,34 +101,52 @@ void readCharacterFile(){
         characters.push_back(line);
     }
     //Read vector into 2D array
-    vector<vector<string>> allCharactersVector;
+    string allCharactersArray[5][6];
     for(int i = 0; i<5; i++){
         stringstream ss(characters[i]);
         string item;
         int j = 0;
         while(getline(ss, item, '|')){
-            allCharactersVector[i][j] = item;
+            allCharactersArray[i][j] = item;
             j++;
         }
         j=0;
     }
     //Read 2D array to character structs
-    /*Character Leo  = {allCharactersArray[0][0], stoi(allCharactersArray[0][1]), stoi(allCharactersArray[0][2]), stoi(allCharactersArray[0][3]), stoi(allCharactersArray[0][4]), stoi(allCharactersArray[0][5])};
+    Character Leo  = {allCharactersArray[0][0], stoi(allCharactersArray[0][1]), stoi(allCharactersArray[0][2]), stoi(allCharactersArray[0][3]), stoi(allCharactersArray[0][4]), stoi(allCharactersArray[0][5])};
     Character Helix  = {allCharactersArray[1][0], stoi(allCharactersArray[1][1]), stoi(allCharactersArray[1][2]), stoi(allCharactersArray[1][3]), stoi(allCharactersArray[1][4]), stoi(allCharactersArray[1][5])};
     Character Panthera  = {allCharactersArray[2][0], stoi(allCharactersArray[2][1]), stoi(allCharactersArray[2][2]), stoi(allCharactersArray[2][3]), stoi(allCharactersArray[2][4]), stoi(allCharactersArray[2][5])};
     Character Adenine  = {allCharactersArray[3][0], stoi(allCharactersArray[3][1]), stoi(allCharactersArray[3][2]), stoi(allCharactersArray[3][3]), stoi(allCharactersArray[3][4]), stoi(allCharactersArray[3][5])};
     Character KMer  = {allCharactersArray[4][0], stoi(allCharactersArray[4][1]), stoi(allCharactersArray[4][2]), stoi(allCharactersArray[4][3]), stoi(allCharactersArray[4][4]), stoi(allCharactersArray[4][5])};
-*/
+    vector<Character> charactersVector = {Leo, Helix, Panthera, Adenine, KMer};
     //Display character menu
-    cout<<"Character selection "<<endl;
-    cout<<"Player 1 choose character:"<<endl;
-    vector<vector<string>> tempAllCharactersVector = allCharactersVector;
+    int player1CharacterNum;
+    int player2CharacterNum;
+    cout<<"Character selection: "<<endl;
+    cout<<"Available characters and stats: "<<endl;
+    cout<<"Name - Experience - Accuracy - Efficiency - Insight - Discovery Points"<<endl<<endl;
     for(int i = 0; i<5; i++){
-        for(int j = 0; j<6; j++){
-            cout<<allCharactersVector[i][j]<<";";
-        }
-        cout<<endl;
+        cout<<"("<<i+1<<")"<<charactersVector[i].name <<" - "<<charactersVector[i].experience<<" - "<<charactersVector[i].accuracy<<" - "<<charactersVector[i].efficiency<<" - "<<charactersVector[i].insight<<" - "<<charactersVector[i].discoveryPoints<<endl;
     }
-
-
+    cout<<endl;
+    //Player 1 chooses character
+    cout<<"Player 1 choose character: ";
+    cin>>player1CharacterNum;
+    cout<<endl;
+    //Remove selected character
+    charactersVector.erase(charactersVector.begin() + player1CharacterNum-1);
+    cout<<"Available characters and stats:"<<endl;
+    cout<<"Name - Experience - Accuracy - Efficiency - Insight - Discovery Points"<<endl<<endl;
+    for(size_t i = 0; i<charactersVector.size(); i++){
+        cout<<"("<<i+1<<")"<<charactersVector[i].name <<" - "<<charactersVector[i].experience<<" - "<<charactersVector[i].accuracy<<" - "<<charactersVector[i].efficiency<<" - "<<charactersVector[i].insight<<" - "<<charactersVector[i].discoveryPoints<<endl;
+    }
+    cout<<endl;
+    //Player 2 chooses character
+    cout<<"Player 2 choose character: ";
+    cin>>player2CharacterNum;
+    cout<<endl;
+    cout<<"All characters selected!"<<endl;
+    //Return a vector with the two selected characters
+    vector<Character> selectedCharacters = {charactersVector[player1CharacterNum-1], charactersVector[player2CharacterNum-1]};
+    return selectedCharacters;
 }
